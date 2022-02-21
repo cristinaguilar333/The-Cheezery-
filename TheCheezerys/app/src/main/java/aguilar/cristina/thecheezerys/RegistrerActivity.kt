72262d00890ec5f41bf89a4c381c39dd168a0e69
cristinaguilar333 <cristina.aguilar216559@potros.itson.edu.mx>
2022-02-20 18:08:55 -0700

    first commit

---
+++ TheCheezerys/app/src/main/java/aguilar/cristina/thecheezerys/RegistrerActivity.kt
@@ -0,0 +1,12 @@
+package aguilar.cristina.thecheezerys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrer)
+    }
+}
